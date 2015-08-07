dd7a5f68ece5b7c1ebfed5d6b8cebe46ee1daf92
z80 <bashkirov.sergey@gmail.com>
2015-08-07 08:13:41 +0300

    All queries work.

--- software/tests/http/src/res/scripts.js
+++ software/tests/http/src/res/scripts.js
@@ -1,5 +1,7 @@
     var image = new Image();
     var timeout;
+    var camX;
+    var camY;
 
     function imgNewName()
     {
@@ -8,35 +10,41 @@
       return res;
     }
 
+    function onInterval()
+    {
+      var auto = $( '#autorefresh' ).prop( "checked" );
+      if ( auto )
+      {
+        var name = imgNewName();
+        image.src = name;
+      }
+    }
+
     function onTimeout( tm )
     {
       clearTimeout( timeout );
-      var name = imgNewName();
-      image.src = name;
+      onInterval()
     }
 
     function loadHandler()
     {
       console.log( "load handler" );
       $( "#img" ).attr( "src", image.src );
-      timeout = setTimeout( onTimeout, 200 );
-    }
-
-    function onPostCamXy()
-    {
-      var x = $( "#slider-horizontal" ).slider( "value" );
+      //timeout = setTimeout( onTimeout, 200 );
+      onInterval()
     }
 
     function documentReady()
     {
-      /*
+      camX = 0;
+      camY = 0;
+
       console.log( "document ready" );
       var name = imgNewName();
       image.onload = loadHandler;
       image.src = name;
-      //setInterval( loadHandler, 1000 );
+      //setInterval( onInterval, 1000 );
       timeout = setTimeout( onTimeout, 1000 );
-      */
 
 
       $( "#send" ).click( function() {
@@ -44,18 +52,7 @@
         data.a = 123;
         data.b = "Hello!";
 
-        var url = window.location.origin;
         var posting = $.post( "dataUrl", data );
-
-
-        /*
-        // Put the results in a div
-        posting.done(function( data ) {
-          var content = $( data ).find( "#content" );
-          $( "#result" ).empty().append( content );
-        });
-        */
-        //window.location.origin
       });
 
 
@@ -67,7 +64,9 @@
         max: 90,
         value: 0,
         slide: function( event, ui ) {
+          camY = ui.value;
           $( "#dirY" ).val( ui.value );
+          //cameraPosSend();
           }
         });
         $( "#dirY" ).val( $( "#slider-vertical" ).slider( "value" ) );
@@ -80,10 +79,50 @@
           max: 90,
           value: 0,
           slide: function( event, ui ) {
+            camX = ui.value;
             $( "#dirX" ).val( ui.value );
+            //cameraPosSend();
             }
           });
           $( "#dirX" ).val( $( "#slider-horizontal" ).slider( "value" ) );
+
+        $( '#autorefresh' ).prop( "checked", true );
+        $( '#autorefresh' ).click( onInterval );
+        $( '#force-refresh-image' ).click( forceRefreshImage );
+        $( '#post-camera-orientation' ).click( cameraPosSend );
+        $( '#light' ).click( lightEn )
+    }
+
+    function forceRefreshImage()
+    {
+      $( '#autorefresh' ).prop( "checked", false );
+      var name = imgNewName();
+      var img = $( '#img' ).attr( "src", name );
+      $( '#result' ).text( 'Force image refresh is invoked' );
+    }
+
+    function cameraPosSend()
+    {
+      var data = {}
+      data.camX = camX;
+      data.camY = camY;
+
+      var posting = $.post( "chassis", data );
+      $( '#result' ).text( 'Camera position: ' + camX.toString() + ", " + camY.toString() );
+    }
+
+    function lightEn()
+    {
+      var data = {}
+      data.light = $( '#light' ).prop( "checked" );
+
+      var posting = $.post( "chassis", data );
+      var stri;
+      if ( data.light )
+        stri = "Light is on";
+      else
+        stri = "Light is off";
+      $( '#result' ).text( stri );
     }
 
     console.log( "before setting handler" );
